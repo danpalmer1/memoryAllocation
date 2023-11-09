2cc67a17666fe616befdaabe54b7a3abf680401e
Jared Weber <jaredweber54@gmail.com>
2023-11-09 09:16:47 -0600

    formating updates

--- Driver.java
+++ Driver.java
@@ -5,20 +5,18 @@ public class Driver {
 
     public static void main(String[] args) {
         //TODO:
-        // + make sure current functionality works for new additions to make future implementation easier
-        // + implement memory management functions to support alloc algorithms
-        // + implement best/worst-fit
-        // + implement next-fit
+        // 	+ implement memory management functions to support alloc algorithms
+        // 	+ implement best/worst-fit
+        // 	+ implement next-fit
+		//	+ make loop run until every process has reached 0 time
+		//	+ currently issue is once all processes are allocated (alloc = true) and all processes are finished (mem.isFinished() = true), we get an infinite loop
     	MemoryAllocator mem = new MemoryAllocator();
-    	//TODO:
-		//+ make loop run until every process has reached 0 time
-		//+ currently issue is once all processes are allocated (alloc = true) and all processes are finished (mem.isFinished() = true), we get an infinite loop
 		boolean alloc = false; //init alloc to false because we need to allocate processes
 		int sysTime = 0;
 		while(!alloc || !mem.isFinished()) { //while processes to be allocated or processes are not finished 
-			System.out.println(" | Number of processes: " + mem.procList.size());
 			alloc = true;
-			System.out.println("SysTime= " + sysTime);
+			System.out.println();
+			System.out.println("\n| SystemTime = " + sysTime + " |");
 			for(Process proc : mem.procList) { //check if there are processes that need to be allocated in procList
 				if(!proc.isAlloc()){ //if process hasn't been allocated
 					alloc = false; //set variable to false
@@ -46,8 +44,7 @@ public class Driver {
 				mem.release(p);
 				System.out.println("Process " + p.getId() + " finished and was released");
 			}
-			mem.print_status();
-			mem.showResults(); //shows calculations
+			mem.showResults(); //shows calculations and allocated proccess
 			sysTime++;
 		} 
 		System.out.println("\nFINISHED");
