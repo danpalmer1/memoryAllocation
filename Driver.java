057126ff1efcfb5061c46c03ea3ddb20e5b2fea8
Jared Weber <jaredweber54@gmail.com>
2023-11-19 13:18:14 -0600

    Runs all algorithms

--- Driver.java
+++ Driver.java
@@ -11,21 +11,21 @@ public class Driver {
 		//	+ make loop run until every process has reached 0 time
 		//	+ currently issue is once all processes are allocated (alloc = true) and all processes are finished (mem.isFinished() = true), we get an infinite loop
 		Driver d = new Driver();
-		d.runAlgorithm();
-		// int f, b, w, n;
-		// f = d.runAlgorithm("F");
-		// b = d.runAlgorithm("B");
-		// w = d.runAlgorithm("W");
-		// n = d.runAlgorithm("N");
-		// System.out.println("First-fit: " + f + "\n"
-		// 	+ "Best-fit: " + b + "\n"
-		// 	+ "Worst-fit: " + w + "\n"
-		// 	+ "Next-fit: " + n + "\n");
+		int f,b,w,n;
+		f = d.runAlgorithm("F");
+		b = d.runAlgorithm("B");
+		w = d.runAlgorithm("W");
+		n = d.runAlgorithm("N");
+	
+		 System.out.println("First-fit: " + f + "\n"
+		 	+ "Best-fit: " + b + "\n"
+		 	+ "Worst-fit: " + w + "\n"
+		 	+ "Next-fit: " + n + "\n");
 }
 
 
-public int runAlgorithm(String algo) {
-	MemoryAllocator mem = new MemoryAllocator("config2.txt");
+    public int runAlgorithm(String algo) {
+	MemoryAllocator mem = new MemoryAllocator("config.txt");
 		boolean alloc = false; //init alloc to false because we need to allocate processes
 		int sysTime = 0;
 		while(!alloc || !mem.isFinished()) { //while processes to be allocated or processes are not finished 
@@ -83,46 +83,45 @@ public int runAlgorithm(String algo) {
 		return sysTime;
     }
 
-public int runAlgorithm() {
-	MemoryAllocator mem = new MemoryAllocator("config2.txt");
-		boolean alloc = false; //init alloc to false because we need to allocate processes
-		int sysTime = 0;
-		while(!alloc || !mem.isFinished()) { //while processes to be allocated or processes are not finished 
-			alloc = true;
-			System.out.println();
-			System.out.println("\n| SystemTime = " + sysTime + " |");
-			for(Process proc : mem.procList) { //check if there are processes that need to be allocated in procList
-				if(!proc.isAlloc()){ //if process hasn't been allocated
-					alloc = false; //set variable to false
-					break;
-				}
-			}
-			if(!alloc) { //if processes still need to be allocated
-				for(int i = 0; i < mem.procList.size(); i++) { //go through each process
-					Process p = mem.procList.get(i);
-					if(mem.next_fit(p, mem.procList.get(i).getSize()) > 0) {
-						System.out.println("Successfully allocated " + mem.partList.get(i).getLength() + " KB to " + mem.procList.get(i).getId());
-					} else {
-						System.err.println("Could not allocate"); 
-					}
-				}
-			}	
-			List<Process> release = new ArrayList<Process>();
-			for(Process p : mem.allocMap.keySet()) {
-				if(p.getTime() == 0 && p.isAlloc()){
-						release.add(p);
-						continue;
-					}
-				p.setTime(p.getTime()- 1);
-			}
-			for(Process p : release) {
-				mem.release(p);
-				System.out.println("Process " + p.getId() + " finished and was released");
-			}
-			mem.showResults(); //shows calculations and allocated proccess
-			sysTime++;
-		} 
-		System.out.println("\nFINISHED");
-		return sysTime;
+//		public int runAlgorithm() {
+//	MemoryAllocator mem = new MemoryAllocator("config2.txt");
+//		boolean alloc = false; //init alloc to false because we need to allocate processes
+//		int sysTime = 0;
+//		while(!alloc || !mem.isFinished()) { //while processes to be allocated or processes are not finished 
+//			alloc = true;
+//			System.out.println();
+//			System.out.println("\n| SystemTime = " + sysTime + " |");
+//			for(Process proc : mem.procList) { //check if there are processes that need to be allocated in procList
+//				if(!proc.isAlloc()){ //if process hasn't been allocated
+//					alloc = false; //set variable to false
+//					break;
+//				}
+//			}
+//			if(!alloc) { //if processes still need to be allocated
+//				for(int i = 0; i < mem.procList.size(); i++) { //go through each process
+//					Process p = mem.procList.get(i);
+//					if(mem.next_fit(p, mem.procList.get(i).getSize()) > 0) {
+//						System.out.println("Successfully allocated " + mem.partList.get(i).getLength() + " KB to " + mem.procList.get(i).getId());
+//					} else {
+//						System.err.println("Could not allocate"); 
+//					}
+//				}
+//			}	
+//			List<Process> release = new ArrayList<Process>();
+//			for(Process p : mem.allocMap.keySet()) {
+//				if(p.getTime() == 0 && p.isAlloc()){
+//						release.add(p);
+//						continue;
+//					}
+//				p.setTime(p.getTime()- 1);
+//			}
+//			for(Process p : release) {
+//				mem.release(p);
+//				System.out.println("Process " + p.getId() + " finished and was released");
+//			}
+//			mem.showResults(); //shows calculations and allocated proccess
+//			sysTime++;
+//		} 
+//		System.out.println("\nFINISHED");
+//		return sysTime;
     }
-}
